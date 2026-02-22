8d762de382c2fcd4c54aaf6bd58a4a3d5b60a24d
jesusjbriceno <jesusj.briceno@outlook.es>
2026-02-22 17:26:35 +0100

    fix: add EF Core migration for Project.CreatedByUserId

--- apps/api/src/Edificia.Infrastructure/Persistence/Migrations/20260222162605_AddProjectCreatedByUserId.Designer.cs
+++ apps/api/src/Edificia.Infrastructure/Persistence/Migrations/20260222162605_AddProjectCreatedByUserId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Edificia.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Edificia.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(EdificiaDbContext))]
-    partial class EdificiaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260222162605_AddProjectCreatedByUserId")]
+    partial class AddProjectCreatedByUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
